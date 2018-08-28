2443d29507445dddb309723c4613415b4133ba11
sonicaks <adityakumarsinha2014@gmail.com>
2018-08-28 13:31:10 +0530

    Added Brute Force Attack Auto Key Cipher cpp file

---
+++ lab-2/Brute Force Auto Key Cipher.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string decode(string cipher, int key) {
+    string plain = "";
+    for (int i = 0; i < cipher.length(); i++) {
+        int c = cipher[i] - 'A';
+        int k = key;
+        if (i >= 1) k = plain[i - 1] - 'A';
+        plain += (char)(((c - k) % 26 + 26) % 26 + 'A');
+    }
+    return plain;
+}
+
+int main() {
+    string cipher = "UTMTCMSALHBVONA";
+
+    for (int k = 0; k < 26; k++) {
+        string decipher = decode(cipher, k);
+        cout << decipher << " " << k << "\n";
+    }
+
+    return 0;
+}
