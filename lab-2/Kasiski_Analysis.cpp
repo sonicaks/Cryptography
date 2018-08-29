5deec421ce2f400cc095ca1da1704b477323390e
sonicaks <adityakumarsinha2014@gmail.com>
2018-08-29 18:46:02 +0530

    Removed printFactors and implemented GCD for finding block size

--- lab-2/Kasiski_Analysis.cpp
+++ lab-2/Kasiski_Analysis.cpp
@@ -7,29 +7,15 @@ void createList(string cipher, map<string, vector<int> > &repLetters) {
         string s = cipher.substr(i, 3);
         repLetters[s].push_back(i);
     }
+
     for (int i = 0; i + 4 <= cipher.length(); i++) {
          string s = cipher.substr(i, 4);
          repLetters[s].push_back(i);
     }
 }
 
-void printFactors(int n) {
-    vector<int> v;
-    for (int i = 1; i * i <= n; i++) {
-        if (n % i == 0) {
-            v.push_back(i);
-            int j = n / i;
-            if (i != j) v.push_back(j);
-        }
-    }
-    sort(v.begin(), v.end());
-    for (auto i : v) {
-        cout << "\t" << i;
-    }
-    cout << "\n";
-}
-
 void printRepLetters(map<string, vector<int> > &repLetters) {
+    int g = 0;
     for (auto it : repLetters) {
         if ((int)it.second.size() == 1) continue;
         cout << it.first << "\t->";
@@ -39,9 +25,10 @@ void printRepLetters(map<string, vector<int> > &repLetters) {
             if (diff == -1) diff = i;
             else diff = i - diff;
         }
-        cout << "\t->\t" << diff << "\t->";
-        printFactors(diff);
+        cout << "\t->\t" << diff << "\n";
+        g = __gcd(diff, g);
     }
+    cout << "Guessed block size is: " << g << "\n";
 }
 
 int main() {
